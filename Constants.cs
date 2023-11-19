ea4f18a9cbf90163e88e1ca0face8cee23f9b00d
unknown <yipy214@gmail.com>
2023-11-20 02:57:08 +0800

    Added some more sql constants; made some progress on FileHandler

--- Constants.cs
+++ Constants.cs
@@ -6,7 +6,7 @@ namespace Constants{
         public static class AccessConstants{
             public static class ColumnNames{
                 public static class MainTable{
-                    public const string FEN = "Fen";
+                    public const string P_FEN = "PFen";
                     public const string ALG_MOVE = "AlgebraicMove";
                     public const string HAS_EVAL = "HasEval";
                     public const string EVAL = "eval";
@@ -18,17 +18,25 @@ namespace Constants{
                     public const string GAMES_PLAYED_WTO = "GamesPlayedWithThisOpening";
                 }
                 public static class ColorTable{
-                    public const string FEN = MainTable.FEN;
+                    public const string P_FEN = MainTable.P_FEN;
                     public const string ALG_MOVE = MainTable.ALG_MOVE;
                     public const string MOVE_TIMES = "MoveTimes";
                     public const string FEN_AFTER_MOVE = MainTable.FEN_AFTER_MOVE;
                 }
                 public static class ColorTimesTable{
-                    public const string FEN = MainTable.FEN;
+                    public const string P_FEN = MainTable.P_FEN;
                     public const string NUM_OF_RESPONSES = "NumberOfResponses";
                     public const string TOTAL_TIMES_PLAYED_BY_COMPUTER = "TotalTimesPlayedByComputer";
                     public const string MEAN_TIMES_PLAYED_BY_COMPUTER = "MeanTimesPlayedByComputer";
                 }
+            }
+            public static class TableNames{
+                public const string MAIN_TABLE = "MainTable";
+                public const string WHITE_TABLE = "WhiteTable";
+                public const string WHITE_TIMES_TABLE = "WhiteTimesTable";
+                public const string BLACK_TABLE = "BlackTable";
+                public const string BLACK_TIMES_TABLE = "BlackTimesTable";
+
             }
         }
     }
--- Filehandler.cs
+++ Filehandler.cs
@@ -1,12 +1,27 @@
-namespace FileHandler{
-using Microsoft.Data.SQLite;
-    class FileHandler{
-        public required string FilePath{get; init;}
-        public FileHandler(string filepath){
-            FilePath = filepath;
-            foreach(string command in Constants.DbConstants.Commands.INITIALIZE_DB){
-
-}
-}
+namespace FileHandler
+{
+    using Constants;
+    using Microsoft.Data.Sqlite;
+    class FileHandler
+    {
+        public string FilePath{ get; init; }
+        private protected SqliteConnection connection;
+        public FileHandler(string fp){
+            FilePath = fp;
+            connection = new SqliteConnection($"Data Source={fp}");
+            connection.Open();
+            Init();
+        }
+        void Init(){
+            foreach(string command in DBConstants.Commands.MAKE_TABLES_COMMANDS){
+                ExecuteNonQuery(command);
+            }
+        }
+        void ExecuteNonQuery(string command){
+            using(var mCommand = new SqliteCommand(command, connection)){
+                    mCommand.ExecuteNonQuery();
+            }
+        }
+        SqliteDataReader ExecuteQuery(string command) => new SqliteCommand(command, connection).ExecuteReader();
     }
 }
